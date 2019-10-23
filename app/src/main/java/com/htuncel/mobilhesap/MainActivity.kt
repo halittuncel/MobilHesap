4164e58366695ecbabe0e5fe2c304b86b90c87f7
Halit Tuncel <47631816+SLO-MO-User@users.noreply.github.com>
2019-10-24 00:19:28 +0300

    Added more custom view styles and created modal to add items and validated the fields

--- app/src/main/java/com/htuncel/mobilhesap/MainActivity.kt
+++ app/src/main/java/com/htuncel/mobilhesap/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.htuncel.mobilhesap
 
+import android.app.DatePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.add_item_modal.view.*
+import java.util.*
+import android.widget.EditText
+import java.lang.Exception
+import java.math.BigDecimal
+import java.text.SimpleDateFormat
+import java.text.ParseException
+import java.util.UUID.randomUUID
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var newSpendingButton: Button
     private lateinit var newIncomeButton: Button
     private lateinit var reportButton: Button
     private lateinit var totalCostText: TextView
+    private lateinit var myCalendar: Calendar
 
     // TODO
     // Create SQLite database hold 2 table; income, spending
@@ -23,29 +43,224 @@ class MainActivity : AppCompatActivity() {
         newSpendingButton = findViewById(R.id.new_spending_button)
         reportButton = findViewById(R.id.report_button)
         totalCostText = findViewById(R.id.total_cost_text)
+        myCalendar = Calendar.getInstance()
 
         newIncomeButton.setOnClickListener {
-            openModal("INCOME")
+            onOpenModal("INCOME")
         }
 
         newSpendingButton.setOnClickListener {
-            openModal("SPENDING")
+            onOpenModal("SPENDING")
         }
 
         reportButton.setOnClickListener {
             // TODO
             // Create new activity and redirect user to that activity
-            Toast.makeText(this, "report", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "income", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun openModal(type: String) {
-        // TODO
-        // Create modal and add new spending info or new income info to database
+    private fun onOpenModal(type: String) {
+
+        // Inflate the modal with custom layout
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_item_modal, null)
+
+        // AlertDialogBuilder
+        val mBuilder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Add Item")
+
+        // Show Modal
+        val mAlertDialog = mBuilder.show()
+        mAlertDialog.setCanceledOnTouchOutside(false)
+
         when (type) {
-            "INCOME" -> Toast.makeText(this, "income", Toast.LENGTH_SHORT).show()
-            "SPENDING" -> Toast.makeText(this, "spending", Toast.LENGTH_SHORT).show()
-            else -> return
+            "INCOME" -> {
+                mDialogView.item_name_edit_text.hint = "Gelir Adı"
+                mAlertDialog.setTitle("Gelir Ekle")
+            }
+            "SPENDING" -> {
+                mDialogView.item_name_edit_text.hint = "Gider Adı"
+                mAlertDialog.setTitle("Gider Ekle")
+            }
+            else -> {
+                // Dismiss the modal
+                mAlertDialog.dismiss()
+            }
+        }
+
+        var date = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH, month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateLabel(mDialogView.item_date_edit_text)
+        }
+
+        mDialogView.item_date_edit_text.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                DatePickerDialog(
+                    this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+
+        // Buttons of modal
+        mDialogView.add_item_button.setOnClickListener {
+
+            // Get the values inside the modal
+            val nameText = mDialogView.item_name_edit_text.text.toString()
+            val costText = mDialogView.item_cost_edit_text.text.toString()
+            val dateText = mDialogView.item_date_edit_text.text.toString()
+
+
+            if (!(validateFields(nameText, costText, dateText))) {
+                //Toast.makeText(this, "Check the fields", Toast.LENGTH_SHORT).show()
+                //return@setOnClickListener
+            }
+
+            if (!validateName(nameText)) {
+                mDialogView.item_date_edit_text.setBackgroundResource(R.drawable.custom_button_1)
+                mDialogView.item_cost_edit_text.setBackgroundResource(R.drawable.custom_button_1)
+                mDialogView.item_name_edit_text.setBackgroundResource(R.drawable.custom_button_2)
+                mDialogView.hideKeyboard()
+                Toast.makeText(
+                    this,
+                    "İsim boş olmamalıdır.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            } else if (!validateCost(costText)) {
+                mDialogView.item_name_edit_text.setBackgroundResource(R.drawable.custom_button_1)
+                mDialogView.item_date_edit_text.setBackgroundResource(R.drawable.custom_button_1)
+                mDialogView.item_cost_edit_text.setBackgroundResource(R.drawable.custom_button_2)
+                mDialogView.hideKeyboard()
+                Toast.makeText(
+                    this,
+                    "Tutar sayı olarak doğru bir şekilde girilmelidir.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            } else if (!validateDate(dateText)) {
+                mDialogView.item_cost_edit_text.setBackgroundResource(R.drawable.custom_button_1)
+                mDialogView.item_name_edit_text.setBackgroundResource(R.drawable.custom_button_1)
+                mDialogView.item_date_edit_text.setBackgroundResource(R.drawable.custom_button_2)
+                mDialogView.hideKeyboard()
+                Toast.makeText(
+                    this,
+                    "Tarih gün-ay-yıl formatında olmalıdır. Örnek (16-02-1999) ",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            } else {
+                // Show values in a toast
+                val uniqueID = randomUUID().toString()
+                var message = "id: $uniqueID \n" +
+                        "name: $nameText \n" +
+                        "cost: $costText \n " +
+                        "date: $dateText"
+
+                when (type) {
+                    "INCOME" -> {
+                        message = message.plus("\n INCOME")
+                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+                        // Dismiss the modal
+                        mAlertDialog.dismiss()
+                    }
+                    "SPENDING" -> {
+                        message = message.plus("\n SPENDING")
+                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+                        // Dismiss the modal
+                        mAlertDialog.dismiss()
+                    }
+                    else -> {
+                        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+                        // Dismiss the modal
+                        mAlertDialog.dismiss()
+                    }
+                }
+            }
+        }
+
+        mDialogView.close_modal_button.setOnClickListener {
+            // Dismiss the modal
+            mAlertDialog.dismiss()
+        }
+
+
+    }
+
+    private fun updateLabel(itemDateEditText: EditText) {
+        val myFormat = "dd-MM-yyyy" // "EEE, d MMM yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale("tr"))
+
+        itemDateEditText.setText(sdf.format(myCalendar.time))
+    }
+
+    private fun validateFields(
+        nameText: String,
+        costText: String,
+        dateText: String
+    ): Boolean {
+        if (!validateCost(costText)) {
+            return false
+        }
+        if (!validateName(nameText)) {
+            return false
+        }
+        if (!validateDate(dateText)) {
+            return false
+        }
+        return true
+    }
+
+    private fun validateCost(costText: String): Boolean {
+        try {
+            val cost = costText.toBigDecimal()
+            Log.i("cost", cost.toString())
+            return cost > BigDecimal.ZERO
+        } catch (e: Exception) {
+            Toast.makeText(this, "Miktar kısmı doğru girilmelidir.", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    private fun validateName(nameText: String): Boolean {
+        val validationText = nameText.trim()
+        if (validationText.isBlank() && validationText.isEmpty()) {
+            return false
+        }
+        return true
+    }
+
+    private fun validateDate(dateText: String): Boolean {
+        val validationText = dateText.trim()
+        if (validationText.isBlank() && validationText.isEmpty()) {
+            return false
+        }
+
+        val format = SimpleDateFormat("dd-MM-yyyy")
+
+        // Input to be parsed should strictly follow the defined date format
+        // above.
+        format.isLenient = false
+
+        val date = "09-10-2019"
+        return try {
+            format.parse(date)
+            true
+        } catch (e: ParseException) {
+            false
         }
     }
+
+    fun View.hideKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
+
 }
